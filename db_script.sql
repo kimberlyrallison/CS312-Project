780f09b8e7e0ce2a1dedc7238e752afad7479be8
Muth1212 <156949366+Muth1212@users.noreply.github.com>
2024-12-05 10:31:20 -0700

    Update db_script.sql

--- db_script.sql
+++ db_script.sql
@@ -26,17 +26,47 @@ ALTER TABLE IF EXISTS public.credentials
 -- Add admin information
 INSERT INTO public.credentials
 VALUES ('admin', 'password')
-
+    
+-- Users Table
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(100) UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL
+);
 -- Workout Plans Table
 CREATE TABLE workout_plans (
     plan_id SERIAL PRIMARY KEY,
     user_id INT REFERENCES users(user_id),  
-    goal VARCHAR(255),
-    fitness_level VARCHAR(50),              
-    equipment_required BOOLEAN DEFAULT FALSE,
-    plan_name VARCHAR(100) NOT NULL,         
-    description TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    workout_time DATETIME, 
+    description TEXT
 );
 
 -- Badges Table
+
+-- Meal Logging Table
+CREATE TABLE wellness_log (
+    wellness_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id),
+    meal_name VARCHAR(100),
+    calories INT,
+    weekday VARCHAR(100),
+    Hours INT
+);
+
+-- Goals Table
+CREATE TABLE goals (
+    goal_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id),
+    goal_type VARCHAR(100),
+    goal VARCHAR(100),
+    target_date DATE         
+);
+
+-- Measurments Table
+CREATE TABLE measurements (
+    measurements_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id),
+    weight INT,
+    height INT,
+    age INT
+);
